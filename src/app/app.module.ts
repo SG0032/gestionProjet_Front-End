4f140d8c23d70c000efb366e4a004e91db2a037e
naoufal <guirnaounaoufal@gmail.com>
2022-01-20 00:22:50 +0000

    component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CategorieComponent } from './categorie/categorie.component';
+import { ProjetComponent } from './projet/projet.component';
+import { TacheComponent } from './tache/tache.component';
+import { ResponsableComponent } from './responsable/responsable.component';
+import { CollaborateurComponent } from './collaborateur/collaborateur.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategorieComponent,
+    ProjetComponent,
+    TacheComponent,
+    ResponsableComponent,
+    CollaborateurComponent
   ],
   imports: [
     BrowserModule,
