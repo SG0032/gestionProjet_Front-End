c760b1b1c953a6c7c86de36c4bda03baf9a2fbad
naoufal <guirnaounaoufal@gmail.com>
2022-01-20 10:28:03 +0000

    page front

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ProjetComponent} from "./projet/projet.component";
+import {TacheComponent} from "./tache/tache.component";
+import {CategorieComponent} from "./categorie/categorie.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'projet', component: ProjetComponent },
+  { path: 'tache', component: TacheComponent },
+  { path: 'categorie', component: CategorieComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
